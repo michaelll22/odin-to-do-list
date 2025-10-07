52373ba5dca97d6f622710500b5e4ccd24674d47
johnmichael <91875751+michaelll22@users.noreply.github.com>
2025-10-07 15:46:51 +0800

    add project creation functionality and styles for project items

--- src/index.js
+++ src/index.js
@@ -32,3 +32,35 @@ menu.addEventListener("click", (e) => {
   const projectTitle = document.querySelector("#projectTitle");
   projectTitle.textContent = e.target.textContent;
 });
+
+const saveBtn = document.querySelectorAll(".save");
+saveBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const projectList = document.querySelector(".projects-list");
+    const input = document.querySelector("#projectName");
+    const projectName = input.value;
+
+    const newProjectItem = document.createElement("div");
+    newProjectItem.classList.add("project-item");
+
+    const newProjectBtn = document.createElement("button");
+    newProjectBtn.classList.add("project-btn");
+    newProjectBtn.textContent = projectName;
+
+    const newDeleteBtn = document.createElement("button");
+    newDeleteBtn.classList.add("delete-project");
+
+    const deleteIcon = document.createElement("i");
+    deleteIcon.setAttribute("class", "fas fa-trash");
+    newDeleteBtn.appendChild(deleteIcon);
+
+    newProjectItem.appendChild(newProjectBtn);
+    newProjectItem.appendChild(newDeleteBtn);
+
+    projectList.appendChild(newProjectItem);
+
+    input.value = "";
+    popupProject.style.display = "";
+    popupTask.style.display = "";
+  });
+});
