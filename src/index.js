d47eef1177c79a7b15abae4b73433cf4626a29f2
johnmichael <91875751+michaelll22@users.noreply.github.com>
2025-10-14 23:32:59 +0800

    initialize project with Webpack setup and basic structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import style from "./styles.css";
+import odinImage from "./odin.png";
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
